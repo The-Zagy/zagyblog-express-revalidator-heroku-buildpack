5c0a60d2418d99c1394a62065e7a2310022dba68
Dan Draper <dan@coderdan.co>
2022-01-07 00:46:26 +1100

    Try adding to path instead of symlinking

--- profile/asdf-pnpm.sh
+++ profile/asdf-pnpm.sh
@@ -1,5 +1,9 @@
 
 export ASDF_DIR=$HOME/.asdf
 export ASDF_DATA_DIR=$HOME/.asdf
-export PATH=$HOME/.asdf/bin/:$HOME/.bin/:$PATH
+
+pnpmver=`asdf current pnpm | awk '{print $2}'`
+nodever=`asdf current nodejs | awk '{print $2}'`
+# Ugly hack!
+export PATH=.asdf/installs/pnpm/${pnpmver}/bin/:.asdf/installs/nodejs/${nodever}/bin/":$PATH
 
