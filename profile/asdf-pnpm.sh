a1c9b6efcb83db97aa14908b24db47e63cd8486c
Dan Draper <dan@coderdan.co>
2022-01-07 00:48:20 +1100

    Fixed typo

--- profile/asdf-pnpm.sh
+++ profile/asdf-pnpm.sh
@@ -5,5 +5,6 @@ export ASDF_DATA_DIR=$HOME/.asdf
 pnpmver=`asdf current pnpm | awk '{print $2}'`
 nodever=`asdf current nodejs | awk '{print $2}'`
 # Ugly hack!
-export PATH=.asdf/installs/pnpm/${pnpmver}/bin/:.asdf/installs/nodejs/${nodever}/bin/":$PATH
+export PATH=.asdf/installs/pnpm/${pnpmver}/bin/:.asdf/installs/nodejs/${nodever}/bin/:$PATH
+echo "Setting path to $PATH"
 
